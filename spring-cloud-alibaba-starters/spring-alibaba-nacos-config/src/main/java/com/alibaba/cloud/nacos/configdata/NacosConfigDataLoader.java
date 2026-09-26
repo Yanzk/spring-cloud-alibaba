8b885efebb19286ee436c143769ca97357551a14
Aurora <easybug@foxmail.com>
2026-09-26 23:39:40 +0800

    fix(nacos): treat empty preference as unset in NacosConfigDataLoader

--- spring-cloud-alibaba-starters/spring-alibaba-nacos-config/src/main/java/com/alibaba/cloud/nacos/configdata/NacosConfigDataLoader.java
+++ spring-cloud-alibaba-starters/spring-alibaba-nacos-config/src/main/java/com/alibaba/cloud/nacos/configdata/NacosConfigDataLoader.java
@@ -31,6 +31,7 @@ import com.alibaba.cloud.nacos.NacosPropertySourceRepository;
 import com.alibaba.cloud.nacos.client.NacosPropertySource;
 import com.alibaba.cloud.nacos.parser.NacosDataParserHandler;
 import com.alibaba.cloud.nacos.refresh.NacosSnapshotConfigManager;
+import com.alibaba.cloud.nacos.utils.StringUtils;
 import com.alibaba.nacos.api.config.ConfigService;
 import com.alibaba.nacos.api.exception.NacosException;
 import org.apache.commons.logging.Log;
@@ -138,7 +139,7 @@ public class NacosConfigDataLoader implements ConfigDataLoader<NacosConfigDataRe
 				.bind(prefix + ".config.preference", ConfigPreference.class)
 				.orElse(LOCAL);
 		String specificPreference = resource.getConfig().getPreference();
-		if (specificPreference != null) {
+		if (StringUtils.isNotEmpty(specificPreference)) {
 			try {
 				preference = ConfigPreference.valueOf(specificPreference.toUpperCase(Locale.ROOT));
 			}
